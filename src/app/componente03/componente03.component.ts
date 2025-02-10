9cd219914b49b7cc256039095a03f56b4d43844f
Patricia <patriciastarck@gmail.com>
2025-02-09 21:00:01 -0300

    Iniciando projeto

---
+++ src/app/componente03/componente03.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente03',
+  standalone: true,
+  imports: [],
+  templateUrl: './componente03.component.html',
+  styleUrl: './componente03.component.css'
+})
+export class Componente03Component {
+
+  // Variavel de imagem
+  imagem:string = 'assets/dia.jpeg'
+
+  // Função para alterar a imagem
+
+  alterarImagem() {
+    if(this.imagem === 'assets/dia.jpeg') {
+      this.imagem = 'assets/noite.jpeg'
+    }else {
+      this.imagem = 'assets/dia.jpeg';
+    }
+  }
+
+
+
+}
